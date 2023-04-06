acc4de28d4210d6f33c6463745871ecd2754909e
m-Cho1 <mturtle051@gmail.com>
2023-04-06 18:34:11 +0900

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+
+namespace Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+            PlayGame();
+            EndGame();
+        }
+
+        private static void StartGame()
+        {
+            Console.WriteLine( "Starting the game...");
+            AskForUsersName();
+        }
+
+        static void AskForUsersName()
+        {
+            Console.WriteLine("Asking user for name...");
+        }
+        private static void PlayGame()
+        {
+            Console.WriteLine("Playing the game...");
+            DisplayMaskedWord();
+            AskForLetter();
+        }
+
+        static void DisplayMaskedWord()
+        {
+            Console.WriteLine("Displaying masked word...");
+        }
+        
+        static void AskForLetter()
+        {
+            Console.WriteLine("Asking for letter...");
+        }
+        private static void EndGame()
+        {
+            Console.WriteLine("Game over...");
+        }
+    }
+}
